4fac6043f5a33d38809ed6ba6e4210a7dcbeaceb
fxybiao <140057493+fxybiao@users.noreply.github.com>
2023-07-21 17:09:46 +0800

    Update stream-proxy.js

--- api/stream-proxy.js
+++ api/stream-proxy.js
@@ -9,18 +9,18 @@ module.exports = (req, res) => {
   // 设置请求头部
   req.headers['Host'] = new URL(targetUrl).host;
 
-  proxy.on('proxyRes', function (proxyRes, req, res) {
-    var body = [];
-    proxyRes.on('data', function (chunk) {
-        body.push(chunk);
-    });
-    proxyRes.on('end', function () {
-        body = Buffer.concat(body).toString();
-        console.log("res from proxied server:", body);
-        res.end("my response to cli");
-    });
-});
+  //proxy.on('proxyRes', function (proxyRes, req, res) {
+  //  var body = [];
+  //  proxyRes.on('data', function (chunk) {
+  //      body.push(chunk);
+ //   });
+  //  proxyRes.on('end', function () {
+ //       body = Buffer.concat(body).toString();
+ //       console.log("res from proxied server:", body);
+ //       res.end("my response to cli");
+ //   });
+//});
 
   // 将请求转发到目标服务
-  proxy.web(req, res, { selfHandleResponse : true, changeOrigin: true, target: targetUrl });
+  proxy.web(req, res, { changeOrigin: true, target: targetUrl });
 };
