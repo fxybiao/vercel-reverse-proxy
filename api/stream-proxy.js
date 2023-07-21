96743878ec4ca9ad897f39a71650e6ddb8adc54e
fxybiao <140057493+fxybiao@users.noreply.github.com>
2023-07-21 11:46:35 +0800

    Create stream-proxy.js

---
+++ api/stream-proxy.js
@@ -0,0 +1,14 @@
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer();
+
+module.exports = (req, res) => {
+  // 目标服务的 URL
+  const targetUrl = 'https://www.google.com/speech-api/v2/recognize'; // 替换为目标服务的 URL
+
+  // 设置请求头部
+  req.headers['Host'] = new URL(targetUrl).host;
+
+  // 将请求转发到目标服务
+  proxy.web(req, res, { target: targetUrl });
+};
