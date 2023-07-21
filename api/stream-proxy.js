c2a770b2bab3f58a1f3d6930c6823dd498340099
fxybiao <140057493+fxybiao@users.noreply.github.com>
2023-07-21 16:02:10 +0800

    Update stream-proxy.js

--- api/stream-proxy.js
+++ api/stream-proxy.js
@@ -11,9 +11,12 @@ module.exports = (req, res) => {
 
   // 用于保存目标服务的响应数据
   let responseData = '';
-
+  let postData = '';
   // 监听目标服务的响应事件
   proxy.on('proxyRes', (proxyRes) => {
+      req.on('data', chunk => {
+      postData += chunk;
+    });
     // 当目标服务的响应到达时，将数据保存到 responseData 变量中
     proxyRes.on('data', (chunk) => {
       responseData += chunk;
@@ -21,12 +24,13 @@ module.exports = (req, res) => {
 
     // 在响应结束后，将 responseData 返回给客户端
     proxyRes.on('end', () => {
-	   console.log('POST 请求参数:', responseData);
+	   console.log('POST 请求参数:', postData);
+	   console.log('POST 请求结果:', responseData);
       // 设置客户端的响应头部
-      //res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
 
       // 返回响应数据给客户端
-      //res.end(responseData);
+      res.end(responseData);
     });
   });
 
